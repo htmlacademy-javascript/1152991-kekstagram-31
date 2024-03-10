73d23cd8c18b673692c5cb2bfb48a14e00d9f99b
Danil <muravtsev.danil@mail.ru>
2024-03-10 20:55:49 +0300

    добавляет показ комментариев

--- js/render-comments.js
+++ js/render-comments.js
@@ -1,23 +1,43 @@
-import { users } from './render-miniatures.js';
-
-const SocialComments = document.querySelectorAll('.social__comment');
-SocialComments.forEach((comment) => comment.remove());
+const COMMENTS_SHOW_COUNT = 5;
 
 const lisComments = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const currentCommentsCount = document.querySelector('.social__comment-shown-count');
+const TotalComments = document.querySelector('.social__comment-total-count');
+const commentsLoader = document.querySelector('.comments-loader');
 
-const showComments = (i) => {
-  users.forEach(({comments}, index) => {
-    if (index === i) {
-      comments.forEach(({avatar, name, message}) => {
-        const comment = commentTemplate.cloneNode(true);
-        comment.querySelector('.social__picture').src = avatar;
-        comment.querySelector('.social__picture').alt = name;
-        comment.querySelector('.social__text').textContent = message;
-        lisComments.appendChild(comment);
-      });
-    }
-  });
+const renderComments = (comments, idx = 0, cb) => {
+  const commentsLength = comments.length;
+  const visibleCommentsCount = COMMENTS_SHOW_COUNT + (COMMENTS_SHOW_COUNT * idx);
+  const resolvedComments = comments.slice(0, visibleCommentsCount);
+
+  commentsLoader.removeEventListener('click', cb);
+
+  lisComments.innerHTML = '';
+
+  for (const item of resolvedComments) {
+    const comment = commentTemplate.cloneNode(true);
+    comment.querySelector('.social__picture').src = item.avatar;
+    comment.querySelector('.social__picture').alt = item.name;
+    comment.querySelector('.social__text').textContent = item.message;
+
+    currentCommentsCount.textContent = resolvedComments.length;
+    lisComments.append(comment);
+  }
+
+  TotalComments.textContent = commentsLength;
+
+  if (commentsLength < COMMENTS_SHOW_COUNT || commentsLength <= visibleCommentsCount) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const clickHandler = () => {
+    renderComments(comments, idx + 1, clickHandler);
+  };
+
+  commentsLoader.addEventListener('click', clickHandler);
 };
 
 const closeComments = () => {
@@ -25,4 +45,4 @@ const closeComments = () => {
   comments.forEach((item) => item.remove());
 };
 
-export { showComments, closeComments };
+export { renderComments, closeComments };
--- js/user-modal.js
+++ js/user-modal.js
@@ -1,37 +1,23 @@
+import {renderComments, closeComments} from './render-comments.js';
 import { users } from './render-miniatures.js';
-import {showComments, closeComments} from './render-comments.js';
 
-const picturesContainer = document.querySelector('.pictures');
 const mainPicture = document.querySelector('.big-picture');
 const mainPictureCancel = mainPicture.querySelector('.big-picture__cancel');
 const mainPictureImg = mainPicture.querySelector('.big-picture__img img');
 const mainlikes = mainPicture.querySelector('.likes-count');
-const mainShowComments = mainPicture.querySelector('.social__comment-shown-count');
-const mainTotalComments = mainPicture.querySelector('.social__comment-total-count');
+
 const socialCaption = mainPicture.querySelector('.social__caption');
 const pictures = document.querySelectorAll('.picture');
 
-const mainSocialComments = document.querySelector('.social__comments');
-const socialComments = mainSocialComments.querySelectorAll('.social__comment');
-
-const socialCommentCount = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
-socialCommentCount.classList.add('hidden');
-commentsLoader.classList.add('hidden');
-
-const openModal = (pictureIndex) => {
+const openModal = (user) => {
   mainPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
-  users.forEach((user, index) => {
-    if (index === pictureIndex) {
-      mainPictureImg.src = user.url;
-      mainlikes.textContent = user.likes;
-      mainShowComments.textContent = socialComments.length;
-      mainTotalComments.textContent = user.comments.length;
-      socialCaption.textContent = user.description;
-    }
-  });
+  mainPictureImg.src = user.url;
+  mainlikes.textContent = user.likes;
+  socialCaption.textContent = user.description;
+
+  renderComments(user.comments);
 };
 
 const closeModal = () => {
@@ -40,18 +26,15 @@ const closeModal = () => {
   closeComments();
 };
 
-picturesContainer.addEventListener('click', (evt) => {
-  const target = evt.target.parentNode;
+const pictureClickHandler = (evt, i) => {
+  evt.preventDefault();
 
-  if (target.matches('.picture') || target.matches('.picture__info')) {
-    pictures.forEach((picture, i) => {
-      if (target === picture) {
-        openModal(i);
-        showComments(i);
-      }
-    });
-  }
-});
+  const idx = i + 1;
+  const finalUser = users.find((user) => user.id === idx);
+  openModal(finalUser);
+};
+
+pictures.forEach((picture, i) => picture.addEventListener('click', (evt) => pictureClickHandler(evt, i)));
 
 mainPictureCancel.addEventListener('click', () => {
   closeModal();
