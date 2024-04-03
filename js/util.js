94b4bce8e7556dab114e40fd10fb59b98d73fbb1
Danil <muravtsev.danil@mail.ru>
2024-04-03 17:24:32 +0300

    исправляет ошибки

--- js/util.js
+++ js/util.js
@@ -3,19 +3,6 @@ const closeModal = (element) => {
   document.body.classList.remove('modal-open');
 };
 
-const getEffectSelector = (currentInputId) => {
-  const selectors = {
-    'effect-none': 'effects__preview--none',
-    'effect-chrome': 'effects__preview--chrome',
-    'effect-sepia': 'effects__preview--sepia',
-    'effect-marvin': 'effects__preview--marvin',
-    'effect-phobos': 'effects__preview--phobos',
-    'effect-heat': 'effects__preview--heat',
-  };
-
-  return selectors[currentInputId];
-};
-
 const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
 
@@ -28,5 +15,4 @@ const debounce = (callback, timeoutDelay = 500) => {
 export {
   closeModal,
   debounce,
-  getEffectSelector,
 };
